5f2e48b7827f0bcc7e103267543516350fee91e8
amina <aminaM@gmail.com>
2026-03-17 15:02:33 +0000

    added-depend

---
+++ src/main/java/org/example/ecommerce_plateform/entities/utilisateur.java
@@ -0,0 +1,13 @@
+package org.example.ecommerce_plateform.entities;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+@GeneratedValue()
+public class utilisateur {
+    @Id
+    private
+}
