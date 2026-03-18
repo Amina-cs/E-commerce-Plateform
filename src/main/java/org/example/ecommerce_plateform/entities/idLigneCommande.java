44be9d6b5eba9535f7009f7f0e5ce8487e9d08fc
israekor <korchi.israe@etu.uae.ac.ma>
2026-03-18 04:02:55 +0000

    correcion des attributs idLigneCommande

--- src/main/java/org/example/ecommerce_plateform/entities/idLigneCommande.java
+++ src/main/java/org/example/ecommerce_plateform/entities/idLigneCommande.java
@@ -4,25 +4,25 @@ import java.io.Serializable;
 import java.util.Objects;
 
 public class idLigneCommande implements Serializable {
-    private int idCommande;
-    private int idProduit;
+    private int commande;
+    private int produit;
 
     public idLigneCommande(){}
 
-    public idLigneCommande(int idCommande, int idProduit) {
-        this.idCommande = idCommande;
-        this.idProduit = idProduit;
+    public idLigneCommande(int commande, int produit) {
+        this.commande = commande;
+        this.produit = produit;
     }
 
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         idLigneCommande that = (idLigneCommande) o;
-        return idCommande == that.idCommande && idProduit == that.idProduit;
+        return commande == that.commande && produit == that.produit;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(idCommande, idProduit);
+        return Objects.hash(commande, produit);
     }
 }
